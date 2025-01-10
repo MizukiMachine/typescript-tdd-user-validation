fb6f7e3828a44f35b952299c270bcd1eb90cc7e5
Mizuki Tamura <negichanfumakun@gmail.com>
2025-01-10 18:45:38 +0900

    必要な型定義作成

---
+++ src/types.ts
@@ -0,0 +1,15 @@
+export interface UserInput {
+  username: string;
+  email: string;
+  password: string;
+  age: number;
+}
+
+export interface ValidationResult {
+  isValid: boolean;
+  errors: string[];
+}
+
+export interface FieldValidationResult {
+  [field: string]: string[];
+}
