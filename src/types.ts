7cdf94a5f6d33c3f61f6093b7e44c292cec301c3
Mizuki Tamura <negichanfumakun@gmail.com>
2025-01-12 19:17:35 +0900

    不要な型定義削除

--- src/types.ts
+++ src/types.ts
@@ -1,10 +1,4 @@
 // src/types.ts
-export interface UserInput {
-  username: string;
-  email: string;
-  password: string;
-  age: number;
-}
 
 export interface ValidationResult {
   isValid: boolean;
